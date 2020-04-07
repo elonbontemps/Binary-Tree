a1ff16ad345d8e295f271afc0dcfd2f5db9667f0
Elon Bontemps <elonbontemps@Elons-MacBook-Pro.local>
2020-04-06 22:50:07 -0700

    clang?

--- homework/01_review/manager.cpp
+++ homework/01_review/manager.cpp
@@ -13,63 +13,63 @@
 #include <string>
 using namespace std;
 
-void Manager::addPlayer(int &numPlayers, Player *&players, string name, int score) {
-    Player *playerTemp = new Player(score, name);
-    Player *nextList = new Player[numPlayers + 1];
+void Manager::addPlayer(int &numPlayers, Player *&players, string name,
+                        int score) {
+  Player *playerTemp = new Player(score, name);
+  Player *nextList = new Player[numPlayers + 1];
+  for (int i = 0; i < numPlayers; ++i) {
+    nextList[i] = players[i];
+  }
+  if (numPlayers > 9) {
+    int min = 0;
     for (int i = 0; i < numPlayers; ++i) {
-        nextList[i] = players[i];
+      if (players[i].getScore() < players[min].getScore()) {
+        min = i;
+      }
     }
-    if (numPlayers > 9) {
-        int min = 0;
-        for (int i = 0; i < numPlayers; ++i) {
-            if (players[i].getScore() < players[min].getScore()) {
-                min = i;
-            }
-        }
-        nextList[min] = *playerTemp;
-        players = nextList;
-    } else {
-        nextList[numPlayers] = *playerTemp;
-        numPlayers++;
-    }
-    Player *formattedList = new Player[numPlayers];
-    int currMax = 12;
-    int index = 0;
+    nextList[min] = *playerTemp;
+    players = nextList;
+  } else {
+    nextList[numPlayers] = *playerTemp;
+    numPlayers++;
+  }
+  Player *formattedList = new Player[numPlayers];
+  int currMax = 12;
+  int index = 0;
 
-    for (int i = 12; i >= 0; --i) {
-        for (int i = 0; i < numPlayers; ++i) {
-            if (nextList[i].getScore() == currMax) {
-                formattedList[index] = nextList[i];
-                index++;
-            }
-        }
-        currMax--;
+  for (int i = 12; i >= 0; --i) {
+    for (int i = 0; i < numPlayers; ++i) {
+      if (nextList[i].getScore() == currMax) {
+        formattedList[index] = nextList[i];
+        index++;
+      }
     }
-    players = formattedList;
+    currMax--;
+  }
+  players = formattedList;
 }
 
 int Manager::searchPlayers(int numPlayers, Player *players, string target) {
-    for (int i = 0; i < numPlayers; ++i) {
-        if (players[i].getName() == target) {
-            return players[i].getScore();
-        }
+  for (int i = 0; i < numPlayers; ++i) {
+    if (players[i].getName() == target) {
+      return players[i].getScore();
     }
-    return -1;
+  }
+  return -1;
 }
 
 void Manager::removePlayer(int &numPlayers, Player *&players, string target) {
-    Player *newList = new Player[numPlayers - 1];
-    newList = players;
-    int j = 0;
-    for (int i = 0; i < numPlayers; ++i) {
-        if (players[i].getName() == target) {
-            continue;
-        } else {
-            newList[j] = players[i];
-            ++j;
-        }
+  Player *newList = new Player[numPlayers - 1];
+  newList = players;
+  int j = 0;
+  for (int i = 0; i < numPlayers; ++i) {
+    if (players[i].getName() == target) {
+      continue;
+    } else {
+      newList[j] = players[i];
+      ++j;
     }
-    players = newList;
-    numPlayers -= 1;
+  }
+  players = newList;
+  numPlayers -= 1;
 }
-
