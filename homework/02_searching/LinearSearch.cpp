0fbb7741ca3ff6ebfe1f943e74935181e718a6dc
Elon Bontemps <elonbontemps@Elons-MacBook-Pro.local>
2020-04-13 18:47:21 -0700

    initialize

--- homework/02_searching/LinearSearch.cpp
+++ homework/02_searching/LinearSearch.cpp
@@ -3,11 +3,21 @@
  * Assignment: Searching
  *
  * File Name:   LinearSearch.cpp
- * Name:        ?
+ * Name:        Elon Bontemps
  * Course:      CPTR 242
- * Date:        ?
+ * Date:        April 13, 2020
  */
 #include "ItemType.h"
 #include "LinearSearch.h"
 
-// TODO implement search
+// Functions
+void BinarySearch::Search(std::vector<ItemType> list, ItemType key,
+                          int &location, int &comparisons) {
+    // locates the size of list and splits it in half
+    int a = list.size() - 1;
+    int b = 0;
+    int c = 0;
+    comparisons = 0;
+    location = -1;
+
+
