5b7052ecb128fb3224c211b1e7a0d9995d18b532
Elon Bontemps <elonbontemps@Elons-MacBook-Pro.local>
2020-04-13 19:04:08 -0700

    Lets see

--- homework/02_searching/LinearSearch.cpp
+++ homework/02_searching/LinearSearch.cpp
@@ -16,6 +16,21 @@ void LinearSearch::Search(std::vector<ItemType> list, ItemType key,
     initcomparisons = 0;
     initlocation = -1;
 
+    for (int i = 0; i < list.size(); ++i) {
+        ++initcomparisons;
+        switch (key.ComparedTo(list.at(i))) {
+            case LESS:
+                return;
+            case GREATER:
+                break;
+            case EQUAL:
+                location = i;
+                return;
+        }
+    }
+}
+
+
 }
 
 
