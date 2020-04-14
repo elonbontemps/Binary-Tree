66a43e1625cc59a69720e7020569be452289bee1
Elon Bontemps <elonbontemps@Elons-MacBook-Pro.local>
2020-04-13 19:00:57 -0700

    wrong place :(

--- homework/02_searching/LinearSearch.cpp
+++ homework/02_searching/LinearSearch.cpp
@@ -11,28 +11,4 @@
 #include "LinearSearch.h"
 
 
-void BinarySearch::Search(std::vector<ItemType> list, ItemType key,
-                          int &location, int &comparisons) {
-    // locates the size of list and splits it in half
-    int a = list.size() - 1;
-    int b = 0;
-    int c = 0;
-    initLocation = -1;
-    initcompare = 0;
 
-    while (a >= c) {
-        ++comparisons;
-        b = (a + 1) / 2;
-        switch (key.ComparedTo(list.at(b))) {
-            case LESS:
-                a = b - 1;
-                break;
-            case GREATER:
-                c = b + 1;
-                break;
-            case EQUAL:
-                initLocation = b;
-                return;
-        }
-    }
-}
