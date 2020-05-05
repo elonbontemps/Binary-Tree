f8e8aa4bcbcfb79a02f0417ef07e14838083ca19
Elon Bontemps <elonbontemps@Elons-MacBook-Pro.local>
2020-05-04 19:55:40 -0700

    whoops

--- homework/05_stack_and_queue/QueueReplaceItem.cpp
+++ homework/05_stack_and_queue/QueueReplaceItem.cpp
@@ -12,33 +12,19 @@
 #include <iostream>
 
 void QueueType::ReplaceItem(ItemType oldItem, ItemType newItem) {
-  if (this->IsEmpty()) {
-    return;
+  QueueType tempQ;
+  while (!this->IsEmpty()) {
+    ItemType item;
+    this->Dequeue(item);
+    if (item == oldItem) {
+      item = newItem;
+    }
+    tempQ.Enqueue(item);
   }
 
-  void QueueType::ReplaceItem(ItemType oldItem, ItemType newItem) {
-    QueueType tempQ;
-    while (!this->IsEmpty()) {
-      ItemType item;
-      this->Dequeue(item);
-      if (item == oldItem) {
-        item = newItem;
-      }
-      tempQ.Enqueue(item);
-      void QueueType::ReplaceItem(ItemType oldItem, ItemType newItem) {
-        QueueType tempQ;
-        while (!this->IsEmpty()) {
-          ItemType item;
-          this->Dequeue(item);
-          if (item == oldItem) {
-            item = newItem;
-          }
-          tempQ.Enqueue(item);
-        }
-
-        while (!tempQ.IsEmpty()) {
-          ItemType item;
-          tempQ.Dequeue(item);
-          this->Enqueue(item);
-        }
-      }
+  while (!tempQ.IsEmpty()) {
+    ItemType item;
+    tempQ.Dequeue(item);
+    this->Enqueue(item);
+  }
+}
