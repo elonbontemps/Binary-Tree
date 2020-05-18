e3db32e2312c242ba3b1b6bea58af0e53ded6ec8
Elon Bontemps <elonbontemps@Elons-MacBook-Pro.local>
2020-05-18 13:52:06 -0700

    deleteVstart

--- homework/07_graph/DeleteVertex.cpp
+++ homework/07_graph/DeleteVertex.cpp
@@ -11,6 +11,9 @@
 #include "GraphType.h"
 
 // IMPLEMENTATION FOR DeleteVertex HERE
+template <class VertexType>
+void GraphType<VertexType>::DeleteVertex(VertexType vertex1) {
+  int index = IndexIs(vertex1);
 
 // Keep this at the end of the file.
 template class GraphType<int>;
