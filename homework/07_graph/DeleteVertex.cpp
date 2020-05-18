977da7e360b3b965fc12b2421e607df8101abc94
Elon Bontemps <elonbontemps@Elons-MacBook-Pro.local>
2020-05-18 14:33:00 -0700

    deleteVertex

--- homework/07_graph/DeleteVertex.cpp
+++ homework/07_graph/DeleteVertex.cpp
@@ -13,7 +13,21 @@
 // IMPLEMENTATION FOR DeleteVertex HERE
 template <class VertexType>
 void GraphType<VertexType>::DeleteVertex(VertexType vertex1) {
+  //Deletion of Edges
   int index = IndexIs(vertex1);
-
+  for (int i = 0; i < numVertices; i++) {
+    if (edges[index][i]) {
+      DeleteEdge(vertex1, vertices[i]);
+    }
+  }
+  for (int i = 0; i < numVertices; i++) {
+    if (edges[i][index]) {
+      DeleteEdge(vertices[i], vertex1);
+    }
+  }
+  AddUnusedVertex(index);
+  vertices[index] = 0;
+  return;
+}
 // Keep this at the end of the file.
 template class GraphType<int>;
