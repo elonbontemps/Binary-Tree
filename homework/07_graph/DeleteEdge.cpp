64879f9ae251072dd726658845c0843e535f2984
Elon Bontemps <elonbontemps@Elons-MacBook-Pro.local>
2020-05-18 13:50:46 -0700

    deleteedge

--- homework/07_graph/DeleteEdge.cpp
+++ homework/07_graph/DeleteEdge.cpp
@@ -11,6 +11,12 @@
 #include "GraphType.h"
 
 // IMPLEMENTATION FOR DeleteEdge HERE
-
+template <class VertexType>
+void GraphType<VertexType>::DeleteEdge(VertexType vertex1, VertexType vertex2) {
+  int index1 = IndexIs(vertex1);
+  int index2 = IndexIs(vertex2);
+//setting = 0 for DeleteEdge
+  edges[index1][index2] = 0;
+}
 // Keep this at the end of the file.
 template class GraphType<int>;
