dcb28155f85b11d31cdafead1debb2afeb21275a
Elon Bontemps <elonbontemps@Elons-MacBook-Pro.local>
2020-05-30 14:24:16 -0700

    clion

--- projects/02_imdb_bacon_number/ImdbGraph.cpp
+++ projects/02_imdb_bacon_number/ImdbGraph.cpp
@@ -1,13 +1,3 @@
-/*************************************************************************
- *
- * Project: ImdbGraph implementation
- *
- * File Name: ImdbGraph.cpp
- * Name:      ??
- * Course:    CPTR 242
- * Date:      ??
- *
- */
 #include <iostream>
 #include <queue>
 #include <string>
@@ -30,59 +20,90 @@ ImdbGraph::ImdbGraph() {}
 // Destructor
 ImdbGraph::~ImdbGraph() {}
 
-// Function: Adds a vertex to the graph.
-// Pre:  Graph has been initialized.
-// Post: The actor or movie will be added as a vertex.
-//          No duplicate vertics will be created.
 void ImdbGraph::AddVertex(string actorOrMovie) {
-  // TODO Add code here.
+  auto vertices = graph.getVertices();
+  if (vertices->find(actorOrMovie) == vertices->end()) {
+    graph.addVertex(actorOrMovie);
+    graph.getVisualizer(actorOrMovie)->setColor(Color("blue"));
+    graph.getVisualizer(actorOrMovie)->setSize(8);
+    graph.getVisualizer(actorOrMovie)->setOpacity(1.5);
+  }
 }
 
-// Function: Adds a edge to the graph.
-// Pre:  Graph has been initialized.
-//          Vertics have been already added to the graph.
-// Post: The actor movie edge will be added to the graph.
-//          No duplicate edges will be created.
 void ImdbGraph::AddEdge(string actorOrMovie, string movieOrActor) {
-  // TODO Add code here.
+  graph.addEdge(actorOrMovie, movieOrActor, "1");
+  graph.addEdge(movieOrActor, actorOrMovie, "1");
+  graph.getLinkVisualizer(actorOrMovie, movieOrActor)->setColor(Color("red"));
+  graph.getLinkVisualizer(movieOrActor, actorOrMovie)->setColor(Color("red"));
+  graph.getLinkVisualizer(actorOrMovie, movieOrActor)->setThickness(1.5);
+  graph.getLinkVisualizer(movieOrActor, actorOrMovie)->setThickness(1.5);
 }
 
-// Function: Updates the visualization for to vertex.
-// Pre:  Graph has been initialized.
-//          Vertex is in the graph.
-// Post: The vertex will display the new color.
 void ImdbGraph::VisualizeVertex(string actorOrMovie, string color) {
-  // TODO Add code here.
+  graph.getVisualizer(actorOrMovie)->setColor(Color(color));
+  graph.getVisualizer(actorOrMovie)->setSize(50);
+  graph.getVisualizer(actorOrMovie)->setOpacity(1.);
 }
 
-// Function: Updates the visualization for to edge.
-// Pre:  Graph has been initialized.
-//          Edge is in the graph.
-// Post: The edge will display the new color.
 void ImdbGraph::VisualizeEdge(string actorOrMovie, string movieOrActor2,
                               string color) {
-  // TODO Add code here.
+  graph.getLinkVisualizer(actorOrMovie, movieOrActor2)->setColor(Color(color));
+  graph.getLinkVisualizer(movieOrActor2, actorOrMovie)->setColor(Color(color));
+  graph.getLinkVisualizer(actorOrMovie, movieOrActor2)->setThickness(8.);
+  graph.getLinkVisualizer(movieOrActor2, actorOrMovie)->setThickness(8.);
 }
 
-// Function: Resets all visualizations to the default.
-// Pre:  Graph has been initialized.
-// Post: The vertics and edges will display the default color.
 void ImdbGraph::ResetVisualizer() {
-  // TODO Add code here.
+  for (auto &vertex : *graph.getVertices()) {
+    graph.getVisualizer(vertex.first)->setColor(Color("orange"));
+    graph.getVisualizer(vertex.first)->setSize(10);
+    graph.getVisualizer(vertex.first)->setOpacity(1.5);
+
+    auto sl_list = graph.getAdjacencyList(vertex.first);
+    for (auto sle = sl_list; sle != NULL; sle = sle->getNext()) {
+      auto destination = ((Edge<string>)sle->getValue()).to();
+      graph.getLinkVisualizer(vertex.first, destination)
+          ->setColor(Color("green"));
+      graph.getLinkVisualizer(vertex.first, destination)->setThickness(1.);
+    }
+  }
 }
 
-// Function: Calculates the Bacon Number for two actors.
-//           In addition to returning the number, the graph is update to
-//           highlight the path.
-//           https://en.wikipedia.org/wiki/Six_Degrees_of_Kevin_Bacon
-// Pre:  Graph has been initialized.
-//          Vertices are in the graph.
-//          Edges connect the actors in the graph.
-// Post: The number returned will be lowest number of actor connections used to
-// connect them.
-//          The graph will visually show the path to connect the actors.
 int ImdbGraph::GetBaconNumber(string sourceActor, string destinationActor) {
-  // TODO Add code here.
+  bool found = false;
+  queue<string> toCheck;
+  map<string, string> parents;
+  toCheck.push(sourceActor);
+  int baconNumber = 0;
+  while (!toCheck.empty()) {
+    string v = toCheck.front();
+    toCheck.pop();
+    if (v == destinationActor) {
+      found = true;
+      break;
+    }
+    auto adjList = graph.getAdjacencyList(v);
+    for (auto edge = adjList; edge != nullptr; edge = edge->getNext()) {
+      auto dest = edge->getValue().to();
+      if (parents.count(dest) == 0) {
+        parents.emplace(dest, v);
+        toCheck.push(dest);
+      }
+    }
+  }
+  if (found) {
+    string currParent = destinationActor;
+    VisualizeVertex(currParent, "orange");
+    while (currParent != sourceActor) {
+      string parent = parents.at(currParent);
+      LinkVisualizer *edge = graph.getLinkVisualizer(parent, currParent);
+      VisualizeEdge(parent, currParent, "orange");
+      VisualizeVertex(parent, "orange");
+      baconNumber++;
+      currParent = parent;
+    }
+    VisualizeVertex(sourceActor, "orange");
+    return baconNumber;
+  }
   return -1;
 }
-
