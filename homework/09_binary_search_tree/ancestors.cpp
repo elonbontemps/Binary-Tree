7c79850b56b40ef7f344f78950370e38d4df7325
Elon Bontemps <elonbontemps@Elons-MacBook-Pro.local>
2020-05-30 14:54:35 -0700

    first two?

--- homework/09_binary_search_tree/ancestors.cpp
+++ homework/09_binary_search_tree/ancestors.cpp
@@ -3,23 +3,61 @@
  * Homework: Implement a three functions for printing tree ancestors.
  *
  * File Name:  ancestors.cpp
- * Name:       ?
+ * Name:       Elon Bontemps
  * Course:     CPTR 242
- * Date:       ?
+ * Date:       June 1, 2020
  *
  */
 #include "TreeType.h"
 
 void TreeType::AncestorsIterative(ItemType value, std::ofstream &outFile) {
-  // TODO implement ancestors iterative print
+  // Iterative Ancestors
+  TreeNode *tempTree = root;
+  while (tempTree->info != value) {
+    if (value < tempTree->info && tempTree->left != NULL) {
+      outFile << tempTree->info;
+      tempTree = tempTree->left;
+    } else if (value > tempTree->info && tempTree->right != NULL) {
+      outFile << tempTree->info;
+      tempTree = tempTree->right;
+    }
+  }
 }
 
 void PrintAncestorsRecursive(TreeNode *tree, ItemType value,
                              std::ofstream &outFile) {
-  // TODO implement ancestors recursive print
+  // Recursive Ancestors
+  if (tree->info == value) {
+    return;
+  }
+  if (value < tree->info && tree->left != NULL) {
+    outFile << tree->info;
+    tree = tree->left;
+    PrintAncestorsRecursive(tree, value, outFile);
+  } else if (value > tree->info && tree->right != NULL) {
+    outFile << tree->info;
+    tree = tree->right;
+    PrintAncestorsRecursive(tree, value, outFile);
+  }
 }
 
 void PrintAncestorsReverse(TreeNode *tree, ItemType value,
                            std::ofstream &outFile) {
-  // TODO implement ancestors reverse print
+  // Recursive Ancestors
+  if (tree->info == value) {
+    return;
+  }
+  TreeNode *tempTree = tree;
+  ItemType prevValue;
+  while (tempTree->info != value) {
+    if (value < tempTree->info && tempTree->left != NULL) {
+      prevValue = tempTree->info;
+      tempTree = tempTree->left;
+    } else if (value > tempTree->info && tempTree->right != NULL) {
+      prevValue = tempTree->info;
+      tempTree = tempTree->right;
+    }
+  }
+  outFile << prevValue;
+  PrintAncestorsReverse(tree, prevValue, outFile);
 }
