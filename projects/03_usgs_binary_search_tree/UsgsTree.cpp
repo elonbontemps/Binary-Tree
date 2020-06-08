af479325b51a28a575f66db506c3f8ba5bbaa063
Elon Bontemps <elonbontemps@Elons-MacBook-Pro.local>
2020-06-08 08:36:12 -0700

    functions

--- projects/03_usgs_binary_search_tree/UsgsTree.cpp
+++ projects/03_usgs_binary_search_tree/UsgsTree.cpp
@@ -3,9 +3,9 @@
  * Project: UsgsTree implementation
  *
  * File Name: UsgsTree.cpp
- * Name:      ??
+ * Name:      Elon Bontemps
  * Course:    CPTR 242
- * Date:      ??
+ * Date:      June 7, 2020
  *
  */
 #include "UsgsTree.h"
@@ -21,51 +21,132 @@
 
 using namespace bridges;
 
-// Function: Finds the smallest quake magnitude in a BST.
-// Pre:  BST has been initialized and populated.
-// Post: The smallest quake will be visualized and returned.
 EarthquakeUSGS
 UsgsTree::getLargestQuake(BSTElement<float, EarthquakeUSGS> *root,
                           string color) {
-  return nullptr;
+  while (root->getRight() != NULL) {
+    root = root->getRight();
+  }
+  root->setColor(Color(color));
+  return root->getValue();
 }
 
-// Function: Finds the largest quake magnitude in a BST.
-// Pre:  BST has been initialized and populated.
-// Post: The largest quake will be visualized and returned.
 EarthquakeUSGS
 UsgsTree::getSmallestQuake(BSTElement<float, EarthquakeUSGS> *root,
                            string color) {
-  return nullptr;
+  while (root->getLeft() != NULL) {
+    root = root->getLeft();
+  }
+  root->setColor(Color(color));
+  return root->getValue();
 }
 
-// Function: Finds all nodes in the BST that are between the min and max
-// parameters. Pre:  BST has been initialized and populated. Post: Returns the
-// number of quakes in this range and visualizes them.
 int UsgsTree::countRange(float min, float max,
                          BSTElement<float, EarthquakeUSGS> *root,
                          string color) {
-  return 0;
+  BSTElement<float, EarthquakeUSGS> *temp;
+  std::queue<BSTElement<float, EarthquakeUSGS> *> rootQueue;
+  std::set<BSTElement<float, EarthquakeUSGS> *> discoveredSet;
+  rootQueue.push(root);
+  discoveredSet.emplace(root);
+
+  while (!rootQueue.empty()) {
+    root = rootQueue.front();
+    rootQueue.pop();
+    temp = root;
+    while (temp->getLeft() != NULL) {
+      temp = temp->getLeft();
+      if (temp->getRight() != NULL) {
+        rootQueue.push(temp);
+      }
+      if (discoveredSet.find(temp) == discoveredSet.end()) {
+        discoveredSet.emplace(temp);
+      }
+    }
+    temp = root;
+    while (temp->getRight() != NULL) {
+      temp = temp->getRight();
+      if (temp->getLeft() != NULL) {
+        rootQueue.push(temp);
+      }
+      if (discoveredSet.find(temp) == discoveredSet.end()) {
+        discoveredSet.emplace(temp);
+      }
+    }
+  }
+
+  int count = 0;
+  for (BSTElement<float, EarthquakeUSGS> *node : discoveredSet) {
+    if (node->getValue().getMagnitude() >= min &&
+        node->getValue().getMagnitude() <= max) {
+      count++;
+      node->setColor(Color(color));
+    }
+  }
+  return count;
 }
 
-// Function:  Finds all nodes in the BST that in the specified location.
-// Pre:  BST has been initialized and populated.
-// Post: Returns the number of quakes in this location and visualizes them.
 int UsgsTree::countByLocation(string location,
                               BSTElement<float, EarthquakeUSGS> *root,
                               string color) {
   return 0;
 }
 
-// Function: Updates all nodes and edges with a visualization.
-// Pre:  BST has been initialized and populated.
-// Post: Returns the number of quakes in the BST and visualizes them.
 int UsgsTree::countWithStyle(BSTElement<float, EarthquakeUSGS> *root,
                              string colorVertex, string colorEdge) {
-  return 0;
+  int count = 0;
+  stack<BSTElement<float, EarthquakeUSGS> *> stack;
+  BSTElement<float, EarthquakeUSGS> *curNode = root;
+
+  while (!stack.empty() || curNode != nullptr) {
+    if (curNode != nullptr) {
+      stack.push(curNode);
+      curNode = curNode->getLeft();
+    } else {
+      curNode = stack.top();
+      stack.pop();
+      curNode->getVisualizer()->setColor(colorVertex);
+      if (curNode->getLeft() != NULL) {
+        curNode->getLinkVisualizer(curNode->getLeft())->setColor(colorEdge);
+      }
+      if (curNode->getRight() != NULL) {
+        curNode->getLinkVisualizer(curNode->getRight())->setColor(colorEdge);
+      }
+      ++count;
+      curNode = curNode->getRight();
+    }
+  }
+  return count;
 }
 
-// Function: Resets the BST visualization back to the defaults.
-// Pre:  BST has been initialized and populated.
-// Post: The BST has no visualizations.
-void UsgsTree::resetVisualization(BSTElement<float, EarthquakeUSGS> *root) {}
+void UsgsTree::resetVisualization(BSTElement<float, EarthquakeUSGS> *root) {
+  BSTElement<float, EarthquakeUSGS> *temp;
+  std::queue<BSTElement<float, EarthquakeUSGS> *> rootQueue;
+  std::set<BSTElement<float, EarthquakeUSGS> *> discoveredSet;
+  rootQueue.push(root);
+
+  while (!rootQueue.empty()) {
+    root = rootQueue.front();
+    rootQueue.pop();
+    temp = root;
+    while (temp->getLeft() != NULL) {
+      temp->setColor(Color("green"));
+      temp->getLinkVisualizer(temp->getLeft())->setColor(Color("green"));
+      temp = temp->getLeft();
+      if (temp->getRight() != NULL) {
+        rootQueue.push(temp);
+      }
+    }
+    temp->setColor(Color("green"));
+    temp = root;
+    while (temp->getRight() != NULL) {
+      temp->setColor(Color("green"));
+      temp->getLinkVisualizer(temp->getRight())->setColor(Color("green"));
+      temp = temp->getRight();
+      if (temp->getLeft() != NULL) {
+        rootQueue.push(temp);
+      }
+    }
+    temp->setColor(Color("green"));
+  }
+}
