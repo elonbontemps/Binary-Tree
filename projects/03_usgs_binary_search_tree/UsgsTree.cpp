bbff800527f93831144141e13be04c618de51253
Elon Bontemps <elonbontemps@Elons-MacBook-Pro.local>
2020-06-08 09:49:23 -0700

    flip the switch

--- projects/03_usgs_binary_search_tree/UsgsTree.cpp
+++ projects/03_usgs_binary_search_tree/UsgsTree.cpp
@@ -44,98 +44,46 @@ UsgsTree::getSmallestQuake(BSTElement<float, EarthquakeUSGS> *root,
 int UsgsTree::countRange(float min, float max,
                          BSTElement<float, EarthquakeUSGS> *root,
                          string color) {
-  BSTElement<float, EarthquakeUSGS> *temp;
-  std::queue<BSTElement<float, EarthquakeUSGS> *> rootQueue;
-  std::set<BSTElement<float, EarthquakeUSGS> *> discoveredSet;
-  rootQueue.push(root);
-  discoveredSet.emplace(root);
-
-  while (!rootQueue.empty()) {
-    root = rootQueue.front();
-    rootQueue.pop();
-    temp = root;
-    while (temp->getLeft() != NULL) {
-      temp = temp->getLeft();
-      if (temp->getRight() != NULL) {
-        rootQueue.push(temp);
-      }
-      if (discoveredSet.find(temp) == discoveredSet.end()) {
-        discoveredSet.emplace(temp);
-      }
-    }
-    temp = root;
-    while (temp->getRight() != NULL) {
-      temp = temp->getRight();
-      if (temp->getLeft() != NULL) {
-        rootQueue.push(temp);
-      }
-      if (discoveredSet.find(temp) == discoveredSet.end()) {
-        discoveredSet.emplace(temp);
-      }
-    }
+  if (root == NULL) {
+    return 0;
   }
 
-  int count = 0;
-  for (BSTElement<float, EarthquakeUSGS> *node : discoveredSet) {
-    if (node->getValue().getMagnitude() >= min &&
-        node->getValue().getMagnitude() <= max) {
-      count++;
-      node->setColor(Color(color));
-    }
+  int count = countRange(min, max, root->getLeft(), color);
+  if (min <= root->getKey() && root->getKey() <= max) {
+    root->getVisualizer()->setColor(Color(color));
+    root->getVisualizer()->setSize(40);
+    count += 1;
   }
+  count += countRange(min, max, root->getRight(), color);
+
   return count;
 }
+
 
 int UsgsTree::countByLocation(string location,
                               BSTElement<float, EarthquakeUSGS> *root,
                               string color) {
-  BSTElement<float, EarthquakeUSGS> *temp;
-  std::queue<BSTElement<float, EarthquakeUSGS> *> rootQueue;
-  std::set<BSTElement<float, EarthquakeUSGS> *> discoveredSet;
-
-  rootQueue.push(root);
-  discoveredSet.emplace(root);
-
-  while (!rootQueue.empty()) {
-    root = rootQueue.front();
-    rootQueue.pop();
-    temp = root;
-    while (temp->getLeft() != NULL) {
-      temp = temp->getLeft();
-      if (temp->getRight() != NULL) {
-        rootQueue.push(temp);
-      }
-      if (discoveredSet.find(temp) == discoveredSet.end()) {
-        discoveredSet.emplace(temp);
-      }
-    }
-    temp = root;
-    while (temp->getRight() != NULL) {
-      temp = temp->getRight();
-      if (temp->getLeft() != NULL) {
-        rootQueue.push(temp);
-      }
-      if (discoveredSet.find(temp) == discoveredSet.end()) {
-        discoveredSet.emplace(temp);
-      }
-    }
+  if (root == NULL) {
+    return 0;
   }
 
-  int count = 0;
-  for (BSTElement<float, EarthquakeUSGS> *node : discoveredSet) {
-    if (node->getValue().getLocation().find(location) != std::string::npos) {
-      count++;
-      node->setColor(Color(color));
-    }
+  int count = countByLocation(location, root->getLeft(), color);
+  if (root->getValue().getLocation().find(location) != -1) {
+    root->getVisualizer()->setColor(Color(color));
+    root->getVisualizer()->setSize(40);
+    count += 1;
   }
+  count += countByLocation(location, root->getRight(), color);
+
   return count;
 }
 
+
 int UsgsTree::countWithStyle(BSTElement<float, EarthquakeUSGS> *root,
                              string colorVertex, string colorEdge) {
-  BSTElement<float, EarthquakeUSGS> *temp;
-  std::queue<BSTElement<float, EarthquakeUSGS> *> rootQueue;
-  std::set<BSTElement<float, EarthquakeUSGS> *> discoveredSet;
+//   BSTElement<float, EarthquakeUSGS> *temp;
+//   std::queue<BSTElement<float, EarthquakeUSGS> *> rootQueue;
+//   std::set<BSTElement<float, EarthquakeUSGS> *> discoveredSet;
 
   rootQueue.push(root);
   discoveredSet.emplace(root);
@@ -174,8 +122,9 @@ int UsgsTree::countWithStyle(BSTElement<float, EarthquakeUSGS> *root,
   return discoveredSet.size();
 }
 
+
 void UsgsTree::resetVisualization(BSTElement<float, EarthquakeUSGS> *root) {
-  BSTElement<float, EarthquakeUSGS> *temp;
+//   BSTElement<float, EarthquakeUSGS> *temp;
   std::queue<BSTElement<float, EarthquakeUSGS> *> rootQueue;
   std::set<BSTElement<float, EarthquakeUSGS> *> discoveredSet;
   rootQueue.push(root);
